795af81013e001b40a0bfa10d96c52fa143b1a83
mike <github@ravuya.com>
2017-03-30 10:01:46 -0600

    more words for 'scandal'

--- conspire.py
+++ conspire.py
@@ -6,6 +6,7 @@ with open("corpus.yaml") as fp:
 # pick a name
 name = random.choice(blob['names'])
 scandal = random.choice(blob['scandals'])
+scandal_word = random.choice(blob['synonyms-for-scandals'])
 place = random.choice(blob['places'])
 
-print "%s caught in %s scandal in %s" % (name, scandal, place)
+print "%s caught in %s %s in %s" % (name, scandal, scandal_word, place)
